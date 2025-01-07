df7ad0b738e09002251699d58b32435b89dfae2c
Stephane LAZAREVIC <stephanelazarevic042@outlook.com>
2025-01-07 15:53:00 +0100

    création automatique du dossier data pour archives

--- automation/archiver.py
+++ automation/archiver.py
@@ -2,6 +2,16 @@ import shutil
 import os
 from datetime import datetime
 
+def create_directories():
+    # Vérifier si les répertoires existent, sinon les créer
+    directories = ['/data', '/data/raw', '/data/archives']
+    for directory in directories:
+        if not os.path.exiss(directory):
+            print(f"Le dossier {directory} n'existe pas. Création...")
+            os.makedirs(directory)
+        else:
+            print(f"Le dossier {directory} existe déjà.")
+
 def archive_data():
     archive_name = f"data/archives/data_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
     shutil.make_archive(archive_name, 'zip', 'data/raw')
